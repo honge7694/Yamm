cea1b550c73a464385a978a94a995f54c37e7b46
JUNGJIN <ojj824@naver.com>
2022-02-17 01:51:27 +0900

    :lipstick:	 Mainpage index ui 제작 #1

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+export default function Home({images}) {
+  
+  console.log(images)
 
-export default function Home() {
   return (
-    <div>
-      hi
+    <div className="">
+      <div className="bg-main">Header</div>
+      <h1>사진 업로드</h1>
+        <div>Camera</div>
+      <h1>오늘 먹은 음식</h1>
+        <div className="grid gap-4 grid-cols-4">
+          {images.map(image => (
+            <div key={image.id}>
+              <img src={image.url}></img>
+            </div>
+          ))}
+        </div>
+      <h1>식단 분석표</h1>
     </div>
   )
+}
+
+export const getServerSideProps = async() => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/photos?_start=0&_end=4`)
+  const images = await res.json();
+
+  return {
+    props: {
+      images
+    }
+  }
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'main': '#EDA345'
+    }
   },
   plugins: [],
 }
