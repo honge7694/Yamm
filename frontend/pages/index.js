d67e74f24defa1cd9b11487d69c77100a4d4a41d
JUNGJIN <ojj824@naver.com>
2022-02-24 03:28:32 +0900

    ✨ 캡처기능 가이드 Modal 완성

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -13,8 +13,8 @@ export default function Home({images}) {
     className: "center",
     centerMode: true,
     infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
+    centerPadding: "100px",
+    slidesToShow: 1,
     speed: 500
   };
 
