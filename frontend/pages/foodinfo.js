1abec6eb7ff3bbf173886b5ed2cbf7273fd156c9
JUNGJIN <ojj824@naver.com>
2022-02-25 03:07:39 +0900

    :sparkles: foodinfo 페이지로 이미지 데이터 넘기기 구현

--- frontend/pages/foodinfo.js
+++ frontend/pages/foodinfo.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
 import { useRouter } from "next/router";
 
 
 function foodinfo(props) {
+
+  const [ foodImage, setFoodImage ] = useState("")
+  
+  useEffect(() => {
+    const Image = localStorage.getItem('image');
+    setFoodImage(Image)
+  }, [])
+  
   const router = useRouter();
 
-    console.log(router)
   return (
-    <div>
-      foodinfo
+    <div className="container mx-auto h-screen bg-slate-50 rounded-3xl" >
+      <div className="flex flex-col items-center text-center">
+        <div className="w-80 pt-9 ">
+          <img src={foodImage} className="rounded-3xl"></img>
+        </div>
+        <div className="w-full pt-8 ">
+          <p className="h-96 bg-main"></p>
+        </div>
+      </div>
     </div>
   )
 }
