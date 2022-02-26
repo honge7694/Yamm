71d1ed79c1592ff2b0f0e3efc598184be65ae413
seoungho99 <c99@kakao.com>
2022-02-26 11:34:34 +0900

    :sparkles: URL 변경

--- backend/yamm/urls.py
+++ backend/yamm/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import base_views
+from .views import base_views, distinction_views, upload_views, food_views
 
 # app_name = 'yamm'
 
-urlpatterns= [
-    # base_views.py
-    path('', base_views.index, name='index')
+urlpatterns = [
+    path('', base_views.index, name='index'),
+    path('distinction', distinction_views.distinction, name='distinction'),
+    path('upload/img', upload_views.upload, name='upload'),
+    path('upload/info', upload_views.upload_info, name='upload_info'),
+    path('food/eaten', food_views.eaten, name='eaten'),
 ]
