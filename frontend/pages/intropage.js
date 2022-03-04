76afe302743d86011bb54fdf141154eccde9e2fc
JUNGJIN <ojj824@naver.com>
2022-03-04 19:45:39 +0900

    🔀 intropage 제작#1

---
+++ frontend/pages/intropage.js
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from "react";
+
+export default function intropage() {
+
+
+  return ( 
+    <>
+      <div className="w-screen h-screen">
+        <div className="bg-cover w-full h-full flex bg-[url('/public/food_bg.jpg')">
+            <div className="m-auto text-center"><p>오직, 당신만을 위한 영양사</p><p>Yammm을 만나보세요</p></div>
+        </div>
+        <div className="bg- h-full">p2</div>
+        <div className="w-full h-full">p3</div>
+      </div>
+    </> 
+  )
+  }
