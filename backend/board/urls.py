2ecb2395ba76c3f78bc6a1b34bebd0bc24895b6d
JINHO PARK <cake3717@gmail.com>
2022-03-05 22:39:00 +0900

    :sparkles: backend 파일 삭제

--- backend/board/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import board_views
-
-urlpatterns =[
-    path('', board_views.PostList.as_view()),
-    # path('write/', board_views.PostWirte.as_view()),
-    # path('delete/', )
-]
