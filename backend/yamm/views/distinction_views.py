c176dff626d81d13e5cef43c324741acc75da6fd
seoungho99 <c99@kakao.com>
2022-03-06 06:21:29 +0900

    :sparkles: 음식 판별 틀 만들기

--- backend/yamm/views/distinction_views.py
+++ backend/yamm/views/distinction_views.py
@@ -1,17 +1,20 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status, permissions
+from drf_yasg.utils import swagger_auto_schema
 
 
-def distinction(request):
+class distinction(APIView):
     '''
     사진 판별
     '''
-    if request.method == 'POST':
-        image = request.FILES['img']
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        image = request.FILES['image']
 
         # TODO: AI 연동 후 음식 이름 반환
-        name = '돈까스'
+        food_name = '돈까스'
 
-        return JsonResponse({
-            "name": name,
-            "status": 200
-        })
+        response = {"food_name": food_name}
+        return Response(response, status.HTTP_200_OK)
