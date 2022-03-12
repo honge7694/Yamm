c0a90182f3abf88174e7846a584d563acee516f5
[3기-2반] 최성호 <c99@kakao.com>
2022-03-12 04:10:35 +0000

    🐛 버그 수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', '127.0.0.1']
+    domains: ['localhost', '127.0.0.1', 'elice-kdt-ai-3rd-team15.koreacentral.cloudapp.azure.com']
   },
 }
 
